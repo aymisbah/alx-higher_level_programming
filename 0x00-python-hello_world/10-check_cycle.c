43e7a0f7594fc9efd0966c1fc27c31e49950e179
aymisbah <aymanmisba78@gmail.com>
2024-01-07 12:55:51 +0100

    py

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+
 /**
- * Return:1 if cylical
- * /
+ * check_cycle - checks if list is cyclical
+ * @list: pointer to list to check
+ * Return: 1 if cyclical, 0 otherwise
+ */
 int check_cycle(listint_t *list)
 {
 	listint_t *slow = list, *fast = list;
